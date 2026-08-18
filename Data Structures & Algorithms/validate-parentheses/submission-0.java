94a726368444b2c3d037d324ae3d1527c1b93798
Aniket Raj <113756494+AniketRaj24@users.noreply.github.com>
2026-08-18 20:20:38 +0530

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> st = new ArrayDeque<>();
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }else{
+                if(st.isEmpty()){
+                    return false;
+                }
+                 if(ch == ')' && st.peek() == '('){
+                st.pop();
+            }else if(ch == '}' && st.peek() == '{'){
+                st.pop();
+            }else if(ch == ']' && st.peek() == '['){
+                st.pop();
+            }else{
+                return false;
+            }
+            }
+
+        }
+        return st.isEmpty();
+    }
+}
